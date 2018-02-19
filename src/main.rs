84e92cd7595fd43a8fb199cad5144467f381e8d7
Jonathan Daugherty <cygnus@foobox.com>
2018-02-18 18:52:54 -0800

    Add basic sphere intersections

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate samplers;
 
+extern crate nalgebra;
+use nalgebra::{Vector3};
+
 #[derive(Clone)]
 struct Color {
     r: f64,
@@ -52,13 +55,65 @@ impl Image {
     }
 }
 
-fn main() {
-    let w = 40;
-    let h = 40;
-    let mut img = Image::new(w, h, black());
+#[derive(Debug)]
+struct Ray {
+    origin: Vector3<f64>,
+    direction: Vector3<f64>,
+}
 
-    for i in 0..w {
-        img.set_pixel(i, i, white());
+impl Ray {
+    fn point_at_distance(&self, t: f64) -> Vector3<f64> {
+        self.origin + (self.direction * t)
+    }
+}
+
+struct Sphere {
+    center: Vector3<f64>,
+    radius: f64,
+}
+
+impl Sphere {
+    fn hit(&self, r: &Ray) -> bool {
+        let oc = r.origin - self.center;
+        let a = r.direction.dot(&r.direction);
+        let b = 2.0 * oc.dot(&r.direction);
+        let c = oc.dot(&oc) - self.radius * self.radius;
+        let discriminant = b * b - 4.0 * a * c;
+        discriminant > 0.0
+    }
+}
+
+fn hit(s: &Sphere, r: &Ray) -> Color {
+    if s.hit(r) {
+        white()
+    } else {
+        black()
+    }
+}
+
+fn main() {
+    let s = Sphere {
+        center: Vector3::new(0.0, 0.0, -1.0),
+        radius: 0.5,
+    };
+
+    let mut img = Image::new(200, 100, black());
+
+    let lower_left = Vector3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vector3::new(4.0, 0.0, 0.0);
+    let vertical = Vector3::new(0.0, 2.0, 0.0);
+    let o = Vector3::new(0.0, 0.0, 0.0);
+
+    for row in (0..img.height).rev() {
+        for col in 0..img.width {
+            let u = (col as f64) / (img.width as f64);
+            let v = (row as f64) / (img.height as f64);
+            let r = Ray {
+                origin: o,
+                direction: lower_left + horizontal * u + vertical * v,
+            };
+            img.set_pixel(col, row, hit(&s, &r));
+        }
     }
 
     img.print();
