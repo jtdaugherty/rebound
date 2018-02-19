99f5e062fc35dd66422d1fb338c3beba86871a0f
Jonathan Daugherty <cygnus@foobox.com>
2018-02-18 20:35:39 -0800

    samplers: use f64

--- samplers/src/lib.rs
+++ samplers/src/lib.rs
@@ -8,8 +8,8 @@ use rand::SeedableRng;
 
 #[derive(Debug)]
 pub struct Point2d {
-    x: f32,
-    y: f32,
+    pub x: f64,
+    pub y: f64,
 }
 
 pub struct Sampler {
@@ -27,8 +27,8 @@ pub fn new() -> Sampler {
 }
 
 pub fn u_grid_regular(root: usize) -> Vec<Point2d> {
-    let increment = 1.0 / ((root as f32) + 1.0);
-    let range: Vec<f32> = (0..root).map(|i| increment * (i as f32 + 1.0)).collect();
+    let increment = 1.0 / ((root as f64) + 1.0);
+    let range: Vec<f64> = (0..root).map(|i| increment * (i as f64 + 1.0)).collect();
 
     iproduct!(&range, &range).map(
         |(x, y)| Point2d { x: x.clone(), y: y.clone(), }).collect()
@@ -36,11 +36,11 @@ pub fn u_grid_regular(root: usize) -> Vec<Point2d> {
 
 pub fn u_grid_random(s: &mut Sampler, num_samples: u32) -> Vec<Point2d> {
     (0..num_samples).map(
-        |_| Point2d { x: s.rng.next_f32(), y: s.rng.next_f32(), }).collect()
+        |_| Point2d { x: s.rng.next_f64(), y: s.rng.next_f64(), }).collect()
 }
 
 pub fn u_grid_jittered(s: &mut Sampler, root: usize) -> Vec<Point2d> {
-    let increment = 1.0 / ((root as f32) + 1.0);
+    let increment = 1.0 / ((root as f64) + 1.0);
     let lo = -0.5 * increment;
     let hi = 0.5 * increment;
     let regular = u_grid_regular(root);
