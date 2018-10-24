8e68346ba8b1bb746fbb90c21716e09f93a9d676
Jonathan Daugherty <cygnus@foobox.com>
2018-10-24 11:50:19 -0700

    Improve image serialization

--- src/main.rs
+++ src/main.rs
@@ -132,9 +132,9 @@ impl Image {
         for row in &self.pixels {
             for pixel in row {
                 write!(buf, "{} {} {}\n",
-                       (pixel.r * 255.0) as u8,
-                       (pixel.g * 255.0) as u8,
-                       (pixel.b * 255.0) as u8);
+                       (pixel.r * 255.99) as u8,
+                       (pixel.g * 255.99) as u8,
+                       (pixel.b * 255.99) as u8);
             }
         }
     }
