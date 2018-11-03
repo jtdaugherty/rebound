3d1215b62a425cb844c85f0f76a145f9f3984ff1
Jonathan Daugherty <cygnus@foobox.com>
2018-11-03 13:37:50 -0700

    Add sdltest crate in bench

---
+++ bench/sdltest/src/main.rs
@@ -0,0 +1,47 @@
+extern crate sdl2; 
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::rect::Rect;
+use sdl2::image::{LoadTexture, INIT_PNG, INIT_JPG};
+
+use std::time::Duration;
+
+fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let _image_context = sdl2::image::init(INIT_PNG | INIT_JPG).unwrap();
+
+    let window = video_subsystem.window("rust-sdl2 demo", 800, 400)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("../rebound/demo.png").unwrap();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+
+    'running: loop {
+        i = (i + 1) % 255;
+
+        canvas.copy(&texture, None, None).expect("Render failed");
+        canvas.present();
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                    Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                        break 'running
+                    },
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
