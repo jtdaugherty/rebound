c16a7a097a709c862fbe4096e3e1a0a9ee5a10c2
Jonathan Daugherty <cygnus@foobox.com>
2018-11-03 21:30:00 -0700

    Add a second testing scene

--- src/scenes.rs
+++ src/scenes.rs
@@ -12,6 +12,7 @@ use materials::emissive;
 pub fn lookup_scene(name: &String) -> Option<&Fn(&Config) -> Scene> {
     let scenes: Vec<(String, &Fn(&Config) -> Scene)> = vec![
         (String::from("thinlens1"), &build_scene1),
+        (String::from("thinlens2"), &build_scene2),
     ];
 
     scenes.iter()
@@ -67,6 +68,98 @@ fn build_scene1(config: &Config) -> Scene {
     Scene {
         objects: all_objects,
         background: Color::all(0.5),
+        camera: Box::new(cam),
+        config: config.clone(),
+        view_plane: ViewPlane {
+            hres: 800,
+            vres: 400,
+            pixel_size: 1.0,
+        },
+    }
+}
+
+fn build_scene2(config: &Config) -> Scene {
+    let s_front_left = sphere::Sphere {
+        center: Vector3::new(-2.0, 0.5, 0.0),
+        radius: 0.5,
+        material: Box::new(lambertian::Lambertian {
+            albedo: Color::all(1.0),
+        }),
+    };
+
+    let s_front_right = sphere::Sphere {
+        center: Vector3::new(2.0, 0.5, 0.0),
+        radius: 0.5,
+        material: Box::new(lambertian::Lambertian {
+            albedo: Color::all(1.0),
+        }),
+    };
+
+    let s_back_left = sphere::Sphere {
+        center: Vector3::new(-1.0, 0.5, -2.0),
+        radius: 0.5,
+        material: Box::new(lambertian::Lambertian {
+            albedo: Color::all(1.0),
+        }),
+    };
+
+    let s_back_right = sphere::Sphere {
+        center: Vector3::new(1.0, 0.5, -2.0),
+        radius: 0.5,
+        material: Box::new(lambertian::Lambertian {
+            albedo: Color::all(1.0),
+        }),
+    };
+
+    let s_light1 = sphere::Sphere {
+        center: Vector3::new(-5.0, 7.0, 0.0),
+        radius: 5.0,
+        material: Box::new(emissive::Emissive {
+            color: Color::new(1.0, 0.3, 0.3),
+        }),
+    };
+
+    let s_light2 = sphere::Sphere {
+        center: Vector3::new(5.0, 7.0, 0.0),
+        radius: 5.0,
+        material: Box::new(emissive::Emissive {
+            color: Color::new(0.3, 0.3, 1.0),
+        }),
+    };
+
+    let s_ground = plane::Plane {
+        origin: Point3::new(0.0, 0.0, 0.0),
+        normal: Vector3::new(0.0, 1.0, 0.0),
+        material: Box::new(lambertian::Lambertian {
+            albedo: Color::all(1.0),
+        }),
+    };
+
+    let cam = cameras::ThinLensCamera {
+        core: CameraCore::new(
+                  Vector3::new(0.0, 1.1, 15.0),
+                  Vector3::new(0.0, 0.0, -1.0),
+                  Vector3::new(0.0, 1.0, 0.0),
+                  ),
+        vp_distance: 1500.0,
+        zoom_factor: 1.0,
+        focal_plane_distance: 4.0,
+        lens_radius: 0.0,
+    };
+
+    let mut all_objects: Vec<Box<Intersectable>> = vec![
+        Box::new(s_ground),
+        Box::new(s_light1),
+        Box::new(s_light2),
+        Box::new(s_front_left),
+        Box::new(s_front_right),
+        Box::new(s_back_left),
+        Box::new(s_back_right),
+    ];
+
+    Scene {
+        objects: all_objects,
+        background: Color::all(0.0),
         camera: Box::new(cam),
         config: config.clone(),
         view_plane: ViewPlane {
